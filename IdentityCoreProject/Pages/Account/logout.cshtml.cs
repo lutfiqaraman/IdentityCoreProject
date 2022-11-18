6913b384318657d58f6b5cc292161fe9c021f829
lutfiqaraman <lutfiqaraman@gmail.com>
2022-11-18 23:20:12 +0300

    Add logout page

---
+++ IdentityCoreProject/Pages/Account/logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityCoreProject.Pages.Account
+{
+    public class logoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
