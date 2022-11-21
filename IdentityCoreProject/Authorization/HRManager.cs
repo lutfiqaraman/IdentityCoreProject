089f94841c83e5c2bb5c04017cc42a66bed9ea43
lutfiqaraman <lutfiqaraman@gmail.com>
2022-11-21 19:24:16 +0300

    add HRManager

---
+++ IdentityCoreProject/Authorization/HRManager.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace IdentityCoreProject.Authorization
+{
+    public class HRManager : IAuthorizationRequirement
+    {
+        public int ProbationMonth { get; }
+        public HRManager(int probationMonth)
+        {
+            ProbationMonth = probationMonth;
+        }
+
+        public class HRManagerHandler : AuthorizationHandler<HRManager>
+        {
+            protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HRManager requirement)
+            {
+                if (!context.User.HasClaim(x => x.Type == "EmploymentDate"))
+                    return Task.CompletedTask;
+
+                DateTime empDate = DateTime.Parse(context.User.FindFirst(x => x.Type == "EmploymentDate").Value);
+                TimeSpan period = DateTime.Now - empDate;
+
+                if (period.Days > 30 * requirement.ProbationMonth)
+                    context.Succeed(requirement);
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
