07cf5ebb5c5e8ce494d6e3723027ccef7d8e7fe3
lutfiqaraman <lutfiqaraman@gmail.com>
2022-11-24 19:50:45 +0300

    add rememberme value

--- IdentityCoreProject/Models/Credential.cs
+++ IdentityCoreProject/Models/Credential.cs
@@ -11,5 +11,8 @@ namespace IdentityCoreProject.Models
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
     }
 }
