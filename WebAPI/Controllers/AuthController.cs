f056709e10afc44f72875d8caaf51f4bb5d194fb
lutfiqaraman <lutfiqaraman@gmail.com>
2022-12-03 12:43:33 +0300

    generate JWT token

--- WebAPI/Controllers/AuthController.cs
+++ WebAPI/Controllers/AuthController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
 using WebAPI.Models;
 
 namespace WebAPI.Controllers
@@ -8,6 +14,13 @@ namespace WebAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IConfiguration Config;
+
+        public AuthController(IConfiguration configuration)
+        {
+            Config = configuration;
+        }
+
         [HttpPost]
         public IActionResult Authenticate([FromBody]Credential credential)
         {
@@ -15,10 +28,46 @@ namespace WebAPI.Controllers
             {
                 if (credential.UserName == "admin" && credential.Password == "password")
                 {
+                    List<Claim>? claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, credential.UserName),
+                        new Claim(ClaimTypes.Email, "admin@mywebsite.com"),
+                        new Claim("HR", "HRDepartment"),
+                        new Claim("Admin", "AdminDepartment"),
+                        new Claim("EmploymentDate", "2022-01-01")
+                    };
+
+                    DateTime expiresAt = DateTime.UtcNow.AddMinutes(30);
+
+                    return Ok(new
+                    {
+                        access_token = GenerateToken(claims, expiresAt),
+                        expires_at = expiresAt,
+                    });
 
                 }
             }
-            
+
+            ModelState.AddModelError("Unauthorized", "You are not authorized to access the endpoint");
+            return Unauthorized(ModelState);
+        }
+
+        private string GenerateToken(IEnumerable<Claim> claims, DateTime expiresAt)
+        {
+
+            byte[]? secretKey =
+                Encoding.UTF8.GetBytes(Config.GetValue<string>("SecretKey"));
+
+            JwtSecurityToken? jwt = new JwtSecurityToken(
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: expiresAt,
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(secretKey), 
+                    SecurityAlgorithms.HmacSha256Signature));
+
+            return 
+                new JwtSecurityTokenHandler().WriteToken(jwt);
         }
     }
 }
